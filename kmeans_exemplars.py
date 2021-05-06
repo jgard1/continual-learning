d7ad940dcc846fc520c9c4be0c22171cbf46348e
Josh <44043414+jgard1@users.noreply.github.com>
2021-05-06 18:42:54 -0400

    yeetos absurdo

--- kmeans_exemplars.py
+++ kmeans_exemplars.py
@@ -52,6 +52,7 @@ class ExemplarHandler(nn.Module, metaclass=abc.ABCMeta):
         logging.info("centroid = "+str(centroid))
         distances = torch.sqrt(torch.sum((cluster_features - centroid) ** 2, dim=1)) 
         logging.info("distances = "+str(distances))
+        logging.info("get_point_nearest_centroid: cluster_features.shape = "+str(cluster_features.shape))
         logging.info("distances.shape = "+str(distances.shape))
         _, min_index = torch.min(distances.unsqueeze())
 
