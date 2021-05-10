27f126dd6b4029657fc5ccadbb274b5e1471a475
Josh <44043414+jgard1@users.noreply.github.com>
2021-05-10 10:57:36 -0400

    yeet

--- main.py
+++ main.py
@@ -481,9 +481,11 @@ def run(args, verbose=False):
 
         ##############################JOSH MEME MODS###################################################### 
         ##############################JOSH MEME MODS###################################################### 
-        josh_data = test_datasets[0][0:1024]
+        jdata_loader = utils.get_data_loader(test_datasets[0], 128, cuda=model._is_on_cuda())
+        josh_data = jdata_loader[0][0:1024]
         for i in range(1, args.tasks):
-            josh_data = torch.cat(josh_data, test_datasets[i][0:1024])
+            jdata_loader = utils.get_data_loader(test_datasets[i], 128, cuda=model._is_on_cuda())
+            josh_data = torch.cat(josh_data, jdata_loader[i][0:1024])
         # josh_data = torch.stack(test_datasets)
         prec = evaluate.josh_validate(model, josh_data, batch_size=128, verbose=True, allowed_classes=None,
              with_exemplars=False, plot_name=args.plot_sname)
